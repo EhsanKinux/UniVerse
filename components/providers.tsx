dac36f797b142328b577576674b3f2f5a83d06d9
EhsanKinux <ehsankhwentao@gmail.com>
2026-06-20 18:46:20 +0330

    feat: wrap ThemeProvider with QueryProvider in Providers component

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { ThemeProvider } from "@/components/theme-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-      {children}
-    </ThemeProvider>
+    <QueryProvider>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        {children}
+      </ThemeProvider>
+    </QueryProvider>
   );
 }
